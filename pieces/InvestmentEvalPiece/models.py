9035c5b2e69c1ac25825795b3782ad45f1c71dc3
filipchrvala <108301045+filipchrvala@users.noreply.github.com>
2026-02-22 15:12:57 +0100

    Update models.py

--- pieces/InvestmentEvalPiece/models.py
+++ pieces/InvestmentEvalPiece/models.py
@@ -2,18 +2,9 @@ from pydantic import BaseModel, Field
 
 
 class InputModel(BaseModel):
-    kpi_results_csv: str = Field(
-        description="Path to kpi_results.csv from KPIPiece"
-    )
-
-    battery_summary_csv: str = Field(
-        description="Path to battery summary csv (cycles etc.)",
-        default=""
-    )
-
-    investment_config_yml: str = Field(
-        description="Path to investment config yaml"
-    )
+    kpi_results_csv: str = Field(description="From KPIPiece")
+    battery_summary_csv: str = Field(default="/home/shared_storage/battery_summary_TEST.csv", description="Battery summary csv")
+    investment_config_yml: str = Field(default="/home/shared_storage/investment_config.yml", description="Investment config yaml")
 
 
 class OutputModel(BaseModel):
